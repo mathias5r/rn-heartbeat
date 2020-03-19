a8fd129f01687e32f67073f9d13f16b43d3362f3
Raja Osama <dks4mii@gmail.com>
2020-03-19 02:45:30 -0700

    Support for API >= 26

--- android/app/src/main/java/com/rnheartbeat/BootUpReceiver.java
+++ android/app/src/main/java/com/rnheartbeat/BootUpReceiver.java
@@ -5,9 +5,17 @@ import android.content.Context;
 import android.content.Intent;
 
 public class BootUpReceiver extends BroadcastReceiver {
-
-    @Override
-    public void onReceive(Context context, Intent intent) {
+@Override
+public void onReceive(Context context, Intent intent) {
+    if(intent.getAction() == Intent.ACTION_BOOT_COMPLETED){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            //log("Starting the service in >=26 Mode from a BroadcastReceiver")
+            context.startForegroundService(new Intent(context, HeartbeartService.class));
+            return;
+        }
+        //log("Starting the service in < 26 Mode from a BroadcastReceiver")
         context.startService(new Intent(context, HeartbeartService.class));
     }
+
+}
 }
