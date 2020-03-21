392f4ec18af375b84f31c853ac9580a9eed7cced
mathias5r <mathiassilva4@gmail.com>
2020-03-21 11:28:06 -0300

    Upgrade to 0.60.0

--- android/app/src/main/java/com/rnheartbeat/HeartbeartService.java
+++ android/app/src/main/java/com/rnheartbeat/HeartbeartService.java
@@ -7,7 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.IBinder;
-import android.support.v4.app.NotificationCompat;
+import androidx.core.app.NotificationCompat;
 import android.app.NotificationManager;
 import android.app.NotificationChannel;
 import android.os.Build;
--- android/app/src/main/java/com/rnheartbeat/MainApplication.java
+++ android/app/src/main/java/com/rnheartbeat/MainApplication.java
@@ -2,13 +2,12 @@ package com.rnheartbeat;
 
 import android.app.Application;
 
+import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -21,10 +20,12 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-          new HeartbeatPackage()
-      );
+
+      @SuppressWarnings("UnnecessaryLocalVariable")
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      // Packages that cannot be autolinked yet can be added manually here, for example:
+      packages.add(new HeartbeatPackage());
+      return packages;
     }
 
     @Override
